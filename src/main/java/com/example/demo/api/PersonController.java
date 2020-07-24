ce4dcdbf104c03a0aa01ed804e7493e5119cb28f
Magnus Strand <Magnus.Strand@skatteetaten.no>
2020-07-24 09:00:30 +0200

    possibility to update and delete persons

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -19,12 +23,27 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(@RequestBody Person person){
+    public void addPerson(@Valid @NotNull @RequestBody Person person){
         personService.addPerson(person);
     }
 
     @GetMapping
     public List<Person> getAllPeople(){
         return personService.getAllPeople();
+    }
+
+    @GetMapping(path= "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path="{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path="{id}")
+    public void updatePerson(@Valid @NotNull @RequestBody Person personToUpdate, @PathVariable("id") UUID id){
+        personService.updatePerson(id, personToUpdate);
     }
 }
